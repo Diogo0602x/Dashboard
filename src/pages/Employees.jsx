7c81358bb11f4f069787c1f31cd987c1dffe7db4
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-06 17:04:59 -0300

    Employees done

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page } from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Employees' />
+      <GridComponent
+        id='gridcomp'
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page]} />
+      </GridComponent>
+    </div>
   )
 }
 
