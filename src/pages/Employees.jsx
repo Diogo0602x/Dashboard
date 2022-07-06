a4d2c87c3663e0b55abfa882af9f4e750f9c4c94
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-06 17:14:46 -0300

    Data tables done

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page } from '@syncfusion/ej2-react-grids';
+import {GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page} from '@syncfusion/ej2-react-grids';
 
-import { employeesData, employeesGrid } from '../data/dummy';
-import { Header } from '../components';
+import {employeesData, employeesGrid} from '../data/dummy';
+import {Header} from '../components';
 
 const Employees = () => {
   return (
