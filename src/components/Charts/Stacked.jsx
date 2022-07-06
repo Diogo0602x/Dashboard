65836e6aeeef3dea62049b27dece2063d467adef
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-06 16:18:44 -0300

    Revenue Section done

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,9 +1,29 @@
- import React from 'react'
- 
- const Stacked = () => {
-   return (
-     <div>Stacked</div>
-   )
- }
- 
- export default Stacked
+import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const Stacked = ({width, height}) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id='charts'
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{border: {width: 0}}}
+      tooltip={{enable: true}}
+      legendSettings={{background: 'white'}}
+    >
+      <Inject
+        services={[Legend, Category, StackingColumnSeries, Tooltip]}
+      />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  )
+}
+
+export default Stacked
