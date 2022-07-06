134d799a4f0afb024f963b3f2e982521daec7c47
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-06 16:59:27 -0300

    Orders done

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -4,11 +4,23 @@ import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, Context
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
 
-
 const Orders = () => {
   return (
-    <div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Orders' />
+      <GridComponent
+        id='gridcomp'
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
     </div>
   )
 }
