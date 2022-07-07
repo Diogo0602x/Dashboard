42ac8b9c8d5238336b2fa02ef8a707910196b4f9
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-07 11:05:38 -0300

    Kanban done

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import {ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop} from '@syncfusion/ej2-react-schedule';
 
-import { scheduleData } from '../data/dummy';
-import { Header } from '../components';
+import {scheduleData} from '../data/dummy';
+import {Header} from '../components';
 
 const Calendar = () => {
   return (
