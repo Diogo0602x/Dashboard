9f100aef8ac8dafd1bdc374e42753b4a94667d68
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-07 10:59:24 -0300

    Calendar done

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
-import { useStateContext } from '../../contexts/ContextProvider';
 
 const Stacked = ({width, height}) => {
   return (
