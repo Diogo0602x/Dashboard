dd5ae57c8a57a6c2103e673f1364226706c26401
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-07-08 13:12:05 -0300

    LineChart done

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip} from '@syncfusion/ej2-react-charts';
+
+import {lineCustomSerie, LinePrimaryYAxis, LinePrimaryXAxis, lineCustomSeries} from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const LineChart = () => {
+  const {currentMode} = useStateContext();
+
   return (
-    <div>LineChart</div>
+    <ChartComponent
+      id='line-chart'
+      height='420'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{border: {width: 0}}}
+      tooltip={{enable: true}}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => 
+          <SeriesDirective key={index} {...item} />
+        )}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
